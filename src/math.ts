81c91aa747e032b291a653b04a0ab30466b02f51
YutaSugimura <nextc3m@gmail.com>
2022-06-01 21:53:18 +0900

    feat: add new functions

--- src/math.ts
+++ src/math.ts
@@ -2,4 +2,8 @@ export const sum = (a: number, b: number): number => a + b;
 
 export const sub = (a: number, b: number): number => a - b;
 
+export const mul = (a: number, b: number): number => a * b;
+
+export const div = (a: number, b: number): number => a / b;
+
 export const pow = (a: number, b: number): number => a ** b;
