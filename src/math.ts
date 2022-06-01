a6685a90b82dff6082b7d62f89e794436b83530f
Yuta Sugimura <44302124+YutaSugimura@users.noreply.github.com>
2022-06-01 21:48:31 +0900

    feat: add semantic-release (#1)
    * feat: add function
    
    * feat: add semantic-release
    
    * feat: add npm semantic-release

--- src/math.ts
+++ src/math.ts
@@ -1,3 +1,5 @@
 export const sum = (a: number, b: number): number => a + b;
 
+export const sub = (a: number, b: number): number => a - b;
+
 export const pow = (a: number, b: number): number => a ** b;
